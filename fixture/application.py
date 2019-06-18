50c3aee3fa5ec19a9fcd2c37a8b95a1bd5968142
gregarah <32466534+gregarah@users.noreply.github.com>
2019-06-18 23:20:26 +0300

    выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,51 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 class Application:
 
-    def __init__(self):
+    def __init__(self): #конструктор, кот. инициализирует ссылку на драйвер и помощников
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self) #помощник получает ссылку на объект класса application
+        self.group = GroupHelper(self)
 
     def return_to_home_page(self):
         wd = self.wd
         wd.find_element_by_link_text("home page").click()
 
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
     def open_add_contacts_page(self):
         wd = self.wd
         wd.find_element_by_link_text("add new").click()
 
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
 
     def create_contact(self,contact):
         wd = self.wd
