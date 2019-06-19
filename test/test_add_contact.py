a22c6aa7676e40ab71e3ac74a0384e450e944c70
gregarah <32466534+gregarah@users.noreply.github.com>
2019-06-19 08:25:12 +0300

    выделен класс для работы с контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,10 +12,10 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Dmitry", middlename="Y", lastname="Gdjfhks", nickname="jjjjjjjj",
-                            title="kkkkkkkkkkk", company="lllllllllllll", address="shfhkhkdsh", home="lksjdfkljhsf",
-                            mobile="+78342235252", work="38764823764826", fax="342425", email="fgdgd@kjhg.com",
-                            homepage="localhost/addressbook/", birth_year="1908", address2="fgdgfgd", phone2="dghdhd",
-                            bday="1", bmonth="January"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="Dmitry", middlename="Y", lastname="Gdjfhks", nickname="jjjjjjjj",
+                               title="kkkkkkkkkkk", company="lllllllllllll", address="shfhkhkdsh", home="lksjdfkljhsf",
+                               mobile="+78342235252", work="38764823764826", fax="342425", email="fgdgd@kjhg.com",
+                               homepage="localhost/addressbook/", birth_year="1908", address2="fgdgfgd", phone2="dghdhd",
+                               bday="1", bmonth="January"))
+    app.session.logout()
