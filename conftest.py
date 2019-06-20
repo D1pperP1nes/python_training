bdf3f0d8c545bb8fcb2fe512dd150883a9c4e380
gregarah <32466534+gregarah@users.noreply.github.com>
2019-06-20 09:14:44 +0300

    делаем новые тесты без рекордера

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from fixture.application import Application
 
-@pytest.fixture
+@pytest.fixture(scope = "session")
 def app(request): #функция создающая фикстуру
     fixture = Application()
     request.addfinalizer(fixture.destroy)
