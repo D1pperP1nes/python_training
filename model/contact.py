028f632ff3e37195d76756c4b750c79494cae219
gregarah <32466534+gregarah@users.noreply.github.com>
2019-06-26 14:44:34 +0300

    задание 7: вспомогательные методы для заполнения форм - для contact

--- model/contact.py
+++ model/contact.py
@@ -2,7 +2,7 @@
 
 class Contact:
 
-    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax, email, homepage, birth_year, address2, phone2, bday, bmonth):
+    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None, address=None, home=None, mobile=None, work=None, fax=None, email=None, homepage=None, birth_year=None, address2=None, phone2=None, bday=None, bmonth=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,7 +2,7 @@ from model.group import Group
 
 def test_edit_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="edited-group"))#, header="edited-group-header", footer="edited-group-footer"))
+    app.group.edit_first_group(Group(name="edited-group"))
     app.session.logout()
 
 def test_edit_group_header(app):
