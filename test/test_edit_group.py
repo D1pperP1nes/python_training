1481a47a63998ca2223e46ee573c53aec5c4f058
gregarah <32466534+gregarah@users.noreply.github.com>
2019-06-26 13:04:34 +0300

    задание 7: вспомогательные методы для заполнения форм - для group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,6 +1,16 @@
 from model.group import Group
 
-def test_edit_first_group(app):
+def test_edit_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="edited-group1", header="edited-group-header", footer="edited-group-footer"))
+    app.group.edit_first_group(Group(name="edited-group"))#, header="edited-group-header", footer="edited-group-footer"))
+    app.session.logout()
+
+def test_edit_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="edited-group-header"))
+    app.session.logout()
+
+def test_edit_group_footer(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(footer="edited-group-footer"))
     app.session.logout()
