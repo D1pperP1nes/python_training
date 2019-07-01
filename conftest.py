ccf68262fdeb5c7a58f1a1f49ebf29a86991abb2
gregarah <32466534+gregarah@users.noreply.github.com>
2019-07-01 11:30:49 +0300

    Повышаем "интеллектуальность" функций login и logout: проверка текущего состояния сессии

--- conftest.py
+++ conftest.py
@@ -9,17 +9,16 @@ def app(request): #функция создающая фикстуру
     global fixture #объявляем что будем пользоваться этой переменной
     if fixture is None: #если fixture=None, то функцию инициализируем и сразу выполняем логин
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid(): #если фикстура не валидна то перезапускаем
             fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+    fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,6 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="group1", header="group-header", footer="group-footer"))
 
-
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
 
