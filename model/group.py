4c54cc8b431cc5f7c1712eda577bbfe302e078fa
gregarah <32466534+gregarah@users.noreply.github.com>
2019-07-05 12:50:30 +0300

    Проверка равенства объектов и списков

--- model/group.py
+++ model/group.py
@@ -6,3 +6,9 @@ class Group:
         self.header = header
         self.footer = footer
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other): #для сравнения логически, чтобы сравнивались идентификаторы и имена, а не физическое расположение объетов в ячейках памяти. Параметр other - это объект с которым мы должны сравнитьб текущий объект self.
+        return self.id == other.id and self.name == other.name #т.е. теперь будет выполняться сравнение по смыслу, а не по физ. расположению
